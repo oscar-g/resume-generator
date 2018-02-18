e519932d0bbc55c0ad8b1398c6bda13c45932a47
Oscar Gonzalez <oscarg610@gmail.com>
2018-02-18 10:35:58 -0500

    move src

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { writeFile } from 'fs';
 import { renderFile } from 'pug';
 import controller from './lib/controller';
 import * as S from './lib/data-shape';
-import data from './data';
+import data from './../data';
 
 const ctl = new controller(renderFile);
 const output = ctl.render(data);

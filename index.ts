5a5a168fbc57bae321618d6bcbebe5c82c0c2bcf
Oscar Gonzalez <oscarg610@gmail.com>
2018-02-18 10:16:54 -0500

    ts lint

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import { renderFile } from 'pug';
-import Controller from './lib/controller';
+import controller from './lib/controller';
 import * as S from './lib/data-shape';
 import data from './data';
 
-const ctl = new Controller(renderFile);
+const ctl = new controller(renderFile);
 const output = ctl.render(data);
 
-console.log("render output:", output);
+console.log('render output:', output);
--- lib/data-shape.ts
+++ lib/data-shape.ts
@@ -1,60 +1,60 @@
 // main container
 type ControllerData = {
-    bio: Bio;
-    exp: Experience[];
-    proj: Project[];
-    educ: Education[];
-}
+  bio: Bio;
+  exp: Experience[];
+  proj: Project[];
+  edu: Education[];
+};
 
 // sections
 type Bio = {
-    fname: string;
-    lname: string;
-    fullName: string;
-    phone: string;
-    email: string;
-    info: string;
-    title: string;
-}
+  fname: string;
+  lname: string;
+  fullName: string;
+  phone: string;
+  email: string;
+  info: string;
+  title: string;
+};
 
 type Experience = {
-    title: string;
-    company: string;
-    start: MonthYear;
-    end?: MonthYear;
-    info?: string;
-    details?: string[];
-    projects?: Project[];
-}
+  title: string;
+  company: string;
+  start: MonthYear;
+  end?: MonthYear;
+  info?: string;
+  details?: string[];
+  projects?: Project[];
+};
 
 type Education = {
-    school: string;
-    degree: string;
-    grad: MonthYear;
-    details?: string[];
-}
+  school: string;
+  degree: string;
+  grad: MonthYear;
+  details?: string[];
+};
 
 type Project = {
-    title: string;
-    date: MonthYear;
-    company: string;
-    url?: string;
-    info?: string;
-    details?: string[];
-    tech?: string[];
-}
+  title: string;
+  date: MonthYear;
+  company: string;
+  url?: string;
+  info?: string;
+  details?: string[];
+  tech?: string[];
+};
 
 // util
 type MonthYear = {
-    m: number;
-    y: number;
-}
+  m: number;
+  y: number;
+};
 
 export {
-    ControllerData,
-    Bio,
-    Experience,
-    Education,
-    Project,
-    MonthYear,
+  ControllerData,
+  Bio,
+  Experience,
+  Education,
+  Project,
+  MonthYear,
 };

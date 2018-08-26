4f801390a8740aec20779ef9d46ac8b95f4697d8
Oscar Gonzalez <oscarg610@gmail.com>
2018-08-26 08:41:15 -0400

    move source files into src directory

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { writeFile } from 'fs';
 import { renderFile } from 'pug';
-import controller from './lib/controller';
-import * as S from './lib/data-shape';
+import { ControllerData as IResumeData } from './lib/data-shape';
 import data from './../data';
 
-const ctl = new controller(renderFile);
-const output = ctl.render(data);
 
-writeFile('./build/index.html', ctl.render(data), e => console.log);
+export const generateHtml = (data: IResumeData): string => {
+  return renderFile('./views/index.pug', data);
+};
+
+console.log(generateHtml(data));

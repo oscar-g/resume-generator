32763177c0953ec1060047fc728f3325ff2a0ebd
Oscar Gonzalez <oscarg610@gmail.com>
2018-09-04 17:57:38 -0400

    install vue app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+  This is the app!
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue { }
+</script>
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  color #2c3e50
+  margin-top 60px
+  max-width 50em
+
+</style>
